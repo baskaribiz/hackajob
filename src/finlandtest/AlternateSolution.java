52390e8652ddecc3f72a198a787cfb1178c1d284
baskaribiz <baskar.ibiz@gmail.com>
2022-07-06 15:50:16 +0100

    Alternate Solution Updated

--- src/finlandtest/AlternateSolution.java
+++ src/finlandtest/AlternateSolution.java
@@ -1,20 +1,20 @@
 package finlandtest;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class AlternateSolution {
     public static void main(String args[] ) throws Exception {
         String inputData = "4" + "\n"
-        		+ "5 -1 1 2 3 0";
+        		+ "1 5 2 3 4";
         
         // Output the solution to the console
         System.out.println(codeHere(inputData));
     }
     public static int codeHere(String inputData) throws Exception {
-        
-        int COMPARE_VALUE = 0;
-        int closestValue = COMPARE_VALUE;
-        
+                
         // Split inputData with new line char \n to get number of elements in the array and array of integers
         String[] input = inputData.split("\n");
 
@@ -29,30 +29,30 @@ class AlternateSolution {
         if(null == input[1] || "".equals(input[1]) || "null".equalsIgnoreCase(input[1]) || input[1].trim().length() <= 0)
             throw new Exception("Input value is Null or Empty");
 
-        String[] elements = input[1].split(" ");
+        // Converts to integer Array
+        int[] elementsArr = Arrays.stream(input[1].split(" ")).mapToInt(Integer::parseInt).toArray();  
                 
-        Arrays.parallelSort(elements);
+        // Converts to ArrayList
+        List<Integer> elementsList = Arrays.stream(elementsArr).boxed().collect(Collectors.toList());;
         
-        System.out.println(elements[0]);
-
-        for(int i = 0; i < elements.length; i++)
-        {
-            int item = Integer.valueOf(elements[i]);
-
-            if (i == 0)
-                closestValue = item;
-
-            if(item == COMPARE_VALUE)
-                return 0;
-
-            if (Math.abs(item) == Math.abs(closestValue)) {
-               closestValue = Math.abs(item);
-            }
-            else if(item < closestValue){
-                closestValue = item;
-            }
-        }    
-
+        // Sort to Ascending Order
+        Collections.sort(elementsList);
+        
+        // Check for 0 and return as 0 is the closest value to 0 
+        Integer zeroVal = elementsList.stream()
+        		  .filter(item -> item == 0)
+        		  .findAny()
+        		  .orElse(null);
+        
+        if(null != zeroVal && zeroVal == 0)
+        	return 0;
+        
+        // Check the closest element has same positive value and return the positive
+        Integer closestValue = elementsList.stream()
+      		  .filter(item -> item == Math.abs(elementsList.get(0)))
+      		  .findAny()
+      		  .orElse(elementsList.get(0));
+                
         return closestValue;
     }
 }
